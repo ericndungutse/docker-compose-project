c70e8f2cfb9026a98b8ee42cd57744c3a831c3fe
ericndungutse <dav.ndungutse@gmil.com>
2024-02-17 10:25:58 +0200

    integrate backend and frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const res = await axios('http://localhost:3002/products');
+
+        console.log(res.data);
+
+        setProducts(res.data);
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Products</h1>
+        {loading && <h2>Loading....</h2>}
+        {!loading &&
+          products.map((product) => {
+            return (
+              <div key={product.id}>
+                {product.name} : {product.price}
+              </div>
+            );
+          })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 // Importing necessary modules
 const express = require('express');
+const cors = require('cors');
 
 // Create an Express application
 const app = express();
+
+app.use(cors());
 
 // Sample product data
 const products = [
@@ -18,7 +21,7 @@ app.get('/products', (req, res) => {
 });
 
 // Start the server and listen on port 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
